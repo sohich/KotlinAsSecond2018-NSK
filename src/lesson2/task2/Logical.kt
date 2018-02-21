c4d034ea2979fe97200d56640e51eab3a3c8e782
Roman Posokhin <posohin@ccfit.nsu.ru>
2018-02-21 10:40:19 +0700

    Fixed queens  task

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -31,7 +31,7 @@ fun isNumberHappy(number: Int): Boolean {
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = (x1 == x2) || (y1 == y2) ||(abs(x1 - y1) == abs(x2 - y2))
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = (x1 == x2) || (y1 == y2) ||(abs(x1 - x2) == abs(y1 - y2))
 
 /**
  * Средняя
